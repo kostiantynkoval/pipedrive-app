8722033fb896000edc40476214a02f25d50b575f
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2018-10-03 12:59:16 +0300

    store bugs fixed

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+if(process.env.NODE_ENV === 'development') {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+  );
+}
 
 export default store
